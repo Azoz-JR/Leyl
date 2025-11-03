4eeb0e384903618edf252e93abd68ab1b6ceb23d
Abdelaziz Salah <77539943+Azoz-JR@users.noreply.github.com>
2025-11-03 17:41:49 +0100

    Initial Commit

---
+++ Leyl/LeylApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeylApp.swift
+//  Leyl
+//
+//  Created by Abdelaziz Salah on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LeylApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
