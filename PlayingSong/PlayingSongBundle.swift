b60d8d5705ac17f23d6b8c89d36f9c8b7ae11d67
Abdelaziz Salah <77539943+Azoz-JR@users.noreply.github.com>
2025-11-08 12:36:47 +0100

    Add LiveActivity Target

---
+++ PlayingSong/PlayingSongBundle.swift
@@ -0,0 +1,18 @@
+//
+//  PlayingSongBundle.swift
+//  PlayingSong
+//
+//  Created by Abdelaziz Salah on 07/11/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PlayingSongBundle: WidgetBundle {
+    var body: some Widget {
+        PlayingSong()
+        PlayingSongControl()
+        PlayingSongLiveActivity()
+    }
+}
